98469f8f6558a2d9667e53c3be281178821a8f05
Tamino Elgert <elgert.tamino@gmail.com>
2023-07-10 22:28:05 +0200

    Initial commit

---
+++ src/main/java/de/electi/academy/usermanagement/microservice/commentsmanagement/ElectiAcademy2023MicroserviceCommentsManagementApplication.java
@@ -0,0 +1,13 @@
+package de.electi.academy.usermanagement.microservice.commentsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiAcademy2023MicroserviceCommentsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectiAcademy2023MicroserviceCommentsManagementApplication.class, args);
+    }
+
+}
